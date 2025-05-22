42ebaa92fae8153c5b1a38fceab91cbaea7bdb33
andrea <miccijhonandrea@gmail.com>
2025-05-22 11:38:16 +0200

    update

--- Django/tutorial/prova/views.py
+++ Django/tutorial/prova/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
 from prova.forms import  PersonForm
@@ -15,8 +15,7 @@ def people(request):
         person =get_object_or_404(Person,id=id) 
         person.delete()
     context = {"people": people}
-    template = loader.get_template('prova/people.html')
-    return HttpResponse(template.render(context,request))
+    return render(request,"prova/people.html",context)
 
 def person(request,id) :
     person = get_object_or_404(Person,id=id)
@@ -33,8 +32,7 @@ def edit_person(request,id) :
 
     form = PersonForm(instance=person)
     context = {"form": form,"id": id,"labelForm" : "Edit Person","url" : "prova:edit_person"}
-    template = loader.get_template('prova/PersonForm.html')
-    return HttpResponse(template.render(context,request))
+    return render(request,"prova/PersonForm.html",context)
 
 def add_person(request) : 
     if request.method == "POST" : 
